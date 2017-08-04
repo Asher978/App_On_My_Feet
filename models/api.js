36d671b82bd6a366b26f859f3b1556dc71db561d
Asher Shaheen <ashershaheen@gmail.com>
2017-08-04 16:04:19 -0400

    removed google maps and added LeafLet Map for rendering marker. All functional

--- models/api.js
+++ models/api.js
@@ -1,11 +1,8 @@
 const db = require('../db/config');
-
 const Api = {};
 
 Api.findById = (id) => {
-    return db.query(`SELECT lat, log from runs WHERE member_id = $1
+    return db.query(`SELECT lat, log, milesRan from runs WHERE member_id = $1
     `, [id]);
 }
-
-
 module.exports = Api;
---
+++ public/src/map.js
@@ -0,0 +1,38 @@
+let id = $('#idcatcher').data('id');
+var locat=[];
+$(getData = () => {
+    console.log('Jquery and main.js are loaded :)))))');
+    // console.log(`Member ID: ${id}`);
+    $.ajax({
+        url: `http://localhost:3000/api/${id}`,
+        method: 'GET',
+        success: (rundata) => {
+            for (let run of rundata) {
+                let miles = run.milesran.toString();
+                locat.push([run.lat, run.log, miles]);
+            }   
+            console.log(locat)
+            drawMap();  
+        }
+    })
+});
+
+function drawMap () {
+    let map = L.map('mapid').setView([locat[0][0], locat[0][1]], 12);
+    mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; ' +'App On My Feet',
+        maxZoom: 15,
+        minZoom: 10,
+    }).addTo(map);
+    for (var i = 0; i < locat.length; i++) {
+        marker = new L.marker([locat[i][0],locat[i][1]])
+        .bindPopup(locat[i][2]+':miles')
+        .addTo(map);
+    }
+}    
+
+
+  
+
+           
